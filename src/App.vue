cf9951c6180af22eabaa5ca70a5def6e0d06e7ae
Itiwat S <gnx18bit@hotmail.com>
2019-09-18 21:57:42 +0700

    add Google Map in web

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <google-map />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMap from "./components/GoogleMap";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GoogleMap
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyB1hWtRazAMx4rQD-NVGygHVigtCyvBRm0",
+    libraries: "places" // necessary for places input
+  }
+});
 
 new Vue({
   render: h => h(App),
